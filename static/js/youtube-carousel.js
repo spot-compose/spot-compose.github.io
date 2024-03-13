5ad27632ca3e6d6e4f187396a25f468fd4cda788
oliver_lemke <lemke.oliver@icloud.com>
2024-03-13 16:00:11 +0100

    youtube carousel css, js

---
+++ static/js/youtube-carousel.js
@@ -0,0 +1,20 @@
+const videos = [
+    "FIRST_VIDEO_ID",
+    "SECOND_VIDEO_ID",
+    "THIRD_VIDEO_ID",
+    // Add as many video IDs as you want
+];
+
+let currentVideo = 0;
+
+function changeVideo(direction) {
+    currentVideo += direction;
+
+    if (currentVideo >= videos.length) {
+        currentVideo = 0; // Loop back to the first video
+    } else if (currentVideo < 0) {
+        currentVideo = videos.length - 1; // Loop to the last video
+    }
+
+    document.getElementById("videoFrame").src = "https://www.youtube.com/embed/" + videos[currentVideo];
+}
